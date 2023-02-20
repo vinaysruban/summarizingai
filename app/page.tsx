f602a46a83466cd9959b9aa06d36febc18ec7d52
Vinay Saravana Ruban <yanivin77@gmail.com>
2023-02-20 20:36:27 +1000

    fixing previous breaking changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
       <section className="text-center w-100 child:py-2 child:px-16">
         <h1 className="from-neutral-800 text-4xl font-black">Hello World!</h1>
         <h2 className="from-neutral-900 text-xl my-8">
-          This function coverts your input below 👇 into something a reading level of
+          This function converts your input below 👇 into something a reading level of
           your choice ✍!
         </h2>
         <Input />
